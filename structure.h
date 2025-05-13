1196d40b2a09a2b21eaf1f71dfbf7a5f55be031c
daspee <raphael_boutor@yahoo.com>
2025-05-13 22:07:04 +0200

    Create structure.h

---
+++ structure.h
@@ -0,0 +1,14 @@
+#include<constants.h>
+
+typedef struct { //structure pour creer des attaques
+	char *name;
+	int damage, focusdamage, heal, focusheal, poison, stun, confused;
+} Attack;
+
+typedef struct { //structure principale personnage (Bunny) surnommé 'Bot'
+	char *name;
+	int agility, pvmax, stamina, poison, stun, confused, pv, dead, utiliunique, chosen;
+	float defence;
+	Attack normal, special, unique;
+} Bot;
+
